8c5a64ecdd6f02f981210678b1576137395ddd47
Sun Qin <sqlxx@163.com>
2020-03-26 19:04:38 +0800

    delete duplication

---
+++ delete_duplication.py
@@ -0,0 +1,26 @@
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        prev = head
+        current = head.next
+        while current:
+            while current and prev.val == current.val:
+                current = current.next
+
+            prev.next = current
+            if not current:
+                return head
+            
+            prev = current
+            current = current.next
+        
+        return head
+                
+            
